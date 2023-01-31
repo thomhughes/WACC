10290373dbafea01dfe1fc4241358383e09ff0a1
Dhruv Jimulia <dj321@ic.ac.uk>
2023-01-31 23:17:00 +0000

    Completed first implementation of parser

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -1,29 +1,10 @@
 package wacc
 
-import parsley.{Parsley, Success, Failure}
-import parsley.character.{digit, whitespaces}
-import parsley.expr.chain
-import parsley.implicits.character.charLift
-import parsley.combinator.eof
+import parsley.{Success, Failure}
 // import wacc.io.readFile
 import wacc.parser.parse
 
-/*
-object Lexer {
-    private val ws = whitespaces
-
-    private def lexeme[A](t: Parsley[A]) = t <~ ws
-    def fully[A](t: Parsley[A]): Parsley[A] = ws ~> t <~ eof
-
-    lazy val integer = lexeme {
-        digit.foldLeft1[BigInt](0)((n, d) => n * 10 + d.asDigit)
-    }
-}
-*/
-
 object Main {
-    // import Lexer._
-
     def main(args: Array[String]): Unit = {
         println("Hello WACC_42!")
 
--- src/main/scala/wacc/parser.scala
+++ src/main/scala/wacc/parser.scala
@@ -4,76 +4,35 @@ import parsley.Parsley
 
 object parser {
   import parsley.combinator.{many, some}
-  import parsley.character._
   import parsley.Parsley.{attempt, pure}
   import parsley.expr.{chain, precedence, Ops, InfixL}
   import parsley.Result
   import ast._
   import lexer._
   import wacc.lexer.implicits._
-  import parsley.debug._
 
-  /*
-  private lazy val `<program>` = Program("begin" *> many(`<func>`), `<statements>` <* "end")
-  private lazy val `<func>` = Func(`<type>`, `<identifier>`, `<param-list>`, "is" *> `<statements>` <* "end")
+  // TODO: rewrite attempt
+  private lazy val `<program>` = Program("begin" *> many(attempt(`<func>`)), `<statements>` <* "end")
+  private lazy val `<func>` = Func(`<type>`, Identifier(`<identifier>`), `<param-list>`, "is" *> `<statements>` <* "end")
   private lazy val `<param-list>` = enclosing.parens(separators.commaSep(`<param>`))
-  private lazy val `<param>` = Parameter(`<type>`, `<identifier>`)
-  */
-  // TODO: may need to backtrack/consider cases with variable names that conflict with syntax
-  /*
-  private lazy val `<statement>`: Parsley[Statement] = {
-    "skip" #> SkipStatement <|>
-    DeclarationStatement(`<type>`, `<identifier>`, "=" *> `<rvalue>`) <|>
-    AssignmentStatement(`<lvalue>`, '=' *> `<rvalue`>) <|> 
-    "read" #> ReadStatement(`<lvalue>`) <|>    
-    "free" *> FreeStatement(`<expression>`) <|>
-    "return" *> ReturnStatement(`<expression>`) <|>
-    "exit" *> ExitStatement(`<expression>`) <|>
-    "print" *> PrintStatement(`<expression>`) <|>
-    "println" *> PrintLnStatement(`<expression>`) <|>
-    IfStatement("if" *> `<expression>`, "then" *> `<statements>`, "else" *> `<statements>` <* "fi") <|>
-    WhileStatement("while" *> `<expression>`, "do" *> `<statments>` <* "done") <|>
-    BeginStatement("begin" *> `<statements>` <* "end")
-  }
-  */
-  /*
-  private lazy val `<lvalue>` = {
-    `<identifier>` <|>
-    `<array-elem>` <|>
-    `<pair-elem>`
-  }
-  */
-  /*
-  private lazy val `<rvalue>` = {
-    `<expression>` <|>
-    ArrayLiteral(enclosing.brackets(separators.commaSep(`<expression>`))) <|>
-    NewPair("newpair" *> ('(' *> `<expression>`), ',' *> `<expression>` <* ')') <|>
-    FunctionCall("call" *> `<identifier>`, enclosing.parens(separators.commaSep(`<expression>`))) <|>
-    `<pair-elem>`
-  }
-  */
-  /*
-  private lazy val `<pair-elem>` = {
-    ("fst" *> PairElem(Fst, `<lvalue>`))  <|>
-    ("snd" *> PairElem(Snd, `<lvalue>`))
-  }
-  */
+  private lazy val `<param>` = Parameter(`<type>`, Identifier(`<identifier>`))
 
+  // TODO: enclosing brackets do not handle whitespace
   private lazy val `<array-elem>` = ArrayElem(Identifier(`<identifier>`), some(enclosing.brackets(`<expression>`)))
-  
-  lazy val `<base-expression>`: Parsley[Expression] = {
+
+  // TODO: rewrite attempt
+  private lazy val `<base-expression>`: Parsley[Expression] = {
     IntLiteral(number) <|>
     BoolLiteral(("true" #> true) <|> "false" #> false) <|>
     "null" #> PairLiteral <|>
     StringLiteral(`<string>`) <|>
     CharLiteral(ascii) <|> 
     UnaryOpApp(`<unary-op>`, `<expression>`) <|>
-    // TODO: not ideal?
     attempt(`<array-elem>`) <|>
     Identifier(`<identifier>`)
   }
 
-  lazy val `<unary-op>`: Parsley[UnaryOp] = {
+  private lazy val `<unary-op>`: Parsley[UnaryOp] = {
     "!" #> Not <|>
     "-" #> Negation <|>
     "len" #> Len <|>
@@ -84,7 +43,7 @@ object parser {
   private def binopParser(opString: String, binOp: BinaryOp) =
     opString #> ((x:Expression, y:Expression) => BinaryOpApp(binOp, x, y))
 
-  lazy val `<expression>`: Parsley[Expression] = precedence[Expression]("(" *> `<expression>` <* ")", `<base-expression>`)(
+  private lazy val `<expression>`: Parsley[Expression] = precedence[Expression]("(" *> `<expression>` <* ")", `<base-expression>`)(
     Ops(InfixL)(binopParser("*", Mul), binopParser("/", Div), binopParser("%", Mod)),
     Ops(InfixL)(binopParser("+", Plus),binopParser("-", Minus)),
     Ops(InfixL)(
@@ -95,53 +54,48 @@ object parser {
     Ops(InfixL)(binopParser("||", Or))
   )
   
+  private lazy val `<type>` = chain.postfix((`<base-type>` <|> `<pair-type>`), ArrayType <# "[" *> "]")
+  private lazy val `<pair-type>` = "pair" *> (enclosing.parens(PairType(`<pair-elem-type>` <* ",", `<pair-elem-type>`)))
+  private lazy val `<pair-elem-type>` = (chain.postfix(`<base-type>`, ArrayType <# "[" *> "]")) <|> ("pair" #> PairRefType)
+  private lazy val `<base-type>` = ("int" #> IntType) <|> ("bool" #> BoolType) <|> ("char" #> CharType) <|> ("string" #> StringType)
 
-  /*
-  private lazy val `<base-type>`: Parsley[BaseType] = {
-    ("int" #> IntType) <|> 
-    ("bool" #> BoolType) <|> 
-    ("char" #> CharType) <|> 
-    ("string" #> StringType)
+  // TODO: attempt not ideal?
+  private lazy val `<lvalue>`: Parsley[LValue] = {
+    attempt(`<array-elem>`) <|>
+    Identifier(`<identifier>`) <|>
+    `<pair-elem>`
   }
-  */
 
+  private lazy val `<pair-elem>` = {
+    ("fst" *> PairElem(pure(Fst), `<lvalue>`))  <|>
+    ("snd" *> PairElem(pure(Snd), `<lvalue>`))
+  }
 
-  // private lazy val `<program>` = Program("begin" *> many(`<func>`), `<statement>` <* "end")
-  // private lazy val `<func>` = Func(`<type>`, `<identifier>`, `<parameters>`, many(`<statement>`))
-  // private lazy val `<parameters>` = enclosing.brackets(separators.commaSep(`<parameter>`))
-  // private lazy val `<parameter>` = Parameter(`<type>`, `<identifier>`)
-  // // TODO: may need to backtrack/consider cases with variable names that conflict with syntax
-  // private lazy val `<statement>` = {
-  //   DeclarationStatement(`<type>`, `<identifier>`, "=" *> `<rvalue>`) <|>
-  //   AssignmentStatement(`<lvalue>`, "=" *> `<rvalue`>) <|> "read" #> ReadStatement(`<lvalue>`) <|>
-  //   "free" *> FreeStatement(`<expression>`) <|>
-  //   "return" *> ReturnStatement(`<expression>`) <|>
-  //   "exit" *> ExitStatement(`<expression>`) <|>
-  //   // TODO: need to account for println and print conflict
-  //   "print" *> PrintStatement(`<expression>`) <|>
-  //   "println" *> PrintLnStatement(`<expression>`) <|>
-  //   IfStatement("if" *> `<expression>`, "then" *> many(`<statement>`), "else" *> many(`<statement>`) <* "fi") <|>
-  //   WhileStatement("while" *> `<expression>`, "do" *> many(`<statment>`) <* "done") <|>
-  //   BeginStatement("begin" *> many(`<statement>`) <* "end")
-  // }
-  // private lazy val `<rvalue>` = {
-  //   `<expression>` <|> 
-  //   ArrayLiteral(many(`<expression>`)) <|>
-  //   NewPair("newpair" , ) <|>
-  //   FunctionCall("call" *> `<identifier>`, '(' *> many(`<expression>`) <* ')')
-  // }
-  // private lazy val `<expression>` = {
-  //   IntLiteral(number)
-  // <|> BoolLiteral(("true" #> true) <|> "false" #> false) 
+  private lazy val `<rvalue>` = {
+    `<expression>` <|>
+    ArrayLiteral(enclosing.brackets(separators.commaSep(`<expression>`))) <|>
+    NewPair("newpair" *> ("(" *> `<expression>`), "," *> `<expression>` <* ")") <|>
+    FunctionCall("call" *> Identifier(`<identifier>`), enclosing.parens(separators.commaSep(`<expression>`))) <|>
+    `<pair-elem>`
+  }
+
+  private lazy val `<statement>`: Parsley[Statement] = {
+    "skip" *> pure(SkipStatement) <|>
+    DeclarationStatement(`<type>`, Identifier(`<identifier>`), "=" *> `<rvalue>`) <|>
+    AssignmentStatement(`<lvalue>`, "=" *> `<rvalue>`) <|> 
+    "read" *> ReadStatement(`<lvalue>`) <|>
+    "free" *> FreeStatement(`<expression>`) <|>
+    "return" *> ReturnStatement(`<expression>`) <|>
+    "exit" *> ExitStatement(`<expression>`) <|>
+    "print" *> PrintStatement(`<expression>`) <|>
+    "println" *> PrintLnStatement(`<expression>`) <|>
+    IfStatement("if" *> `<expression>`, "then" *> `<statements>`, "else" *> `<statements>` <* "fi") <|>
+    WhileStatement("while" *> `<expression>`, "do" *> `<statements>` <* "done") <|>
+    BeginStatement("begin" *> `<statements>` <* "end")
+  }
   
-  // }
+  private lazy val `<statements>` = separators.semiSep(`<statement>`)
 
-  // lazy val `<type>` = (`<base-type>` <|> `<pair-type>`) <**> (("[]" #> ((x: Type) => ArrayType(x))) <|> pure(identity[Type] _)) 
-  lazy val `<type>` = chain.postfix((`<base-type>` <|> `<pair-type>`), ArrayType <# "[" *> "]")
-  lazy val `<pair-type>` = "pair" *> (enclosing.parens(PairType(`<pair-elem-type>` <* ",", `<pair-elem-type>`)))
-  lazy val `<pair-elem-type>` = (chain.postfix(`<base-type>`, ArrayType <# "[" *> "]")) <|> ("pair" #> PairRefType)
-  lazy val `<base-type>` = ("int" #> IntType) <|> ("bool" #> BoolType) <|> ("char" #> CharType) <|> ("string" #> StringType)
-
-  def parse(input: String): Result[String, Expression] =
-    `<expression>`.parse(input)
+  def parse(input: String): Result[String, Program] =
+    `<program>`.parse(input)
 }
