eedacbeab8bbb81056da9c2cda50aec06a704af8
Sachin Wadhwani <sachin.wadhwani21@imperial.ac.uk>
2023-02-03 12:52:17 +0000

    Added integration tests using wacc_examples, changed main script to suppress output for integration tests, updated yml file to clone wacc_examples to allow running of test suite

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -3,18 +3,29 @@ package wacc
 import parsley.{Success, Failure}
 // import wacc.io.readFile
 import wacc.parser.parse
+import wacc.parser.parseExpression
 import wacc.io.readFile
 
 object Main {
     def main(args: Array[String]): Unit = {
+        // parseExpression(args.head) match {
+        //     case Success(x) => {
+        //         println(x)
+        //     }
+        //     case Failure(msg) => {
+        //         println(msg)
+        //     }
+        // }
         parse(readFile(args.head)) match {
             case Success(x) => {
-                println(x)
-                //System.err.println("exit:\n0")
+                // println(x)
+                // System.err.println("exit:\n0")
                 sys.exit(0)
             }
             case Failure(msg) => {
-                System.err.println("#syntax error#")
+                if (args(1) != "--suppress") {
+                    System.err.println("#syntax error#")
+                }
                 // This is for us during debugging
                 // println(msg)
                 // System.err.println("exit:\n100")
---
+++ src/test/scala/wacc/IntegrationTest.scala
@@ -0,0 +1,32 @@
+package wacc
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class IntegrationTest extends AnyFlatSpec {
+    import java.nio.file.{Files, Paths, Path}
+    import scala.jdk.CollectionConverters._
+    import org.scalatest.matchers.should.Matchers._
+    import scala.sys.process._
+
+    val directoriesToRunTestsOn = List("valid")
+
+    directoriesToRunTestsOn.foreach(runTestOnDirectory)
+    
+    def runTest(path: Path): Unit = {
+        val result = ("./compile " + path.toString() + " --suppress").!
+        var exitCode = 0
+        if (path.toString().startsWith("wacc_examples/invalid/syntaxErr")) {
+            exitCode = 100
+        } else if (path.toString().startsWith("wacc_examples/invalid/semanticErr")) {
+            exitCode = 200
+        }
+        "Parsing " + path.toString() + " test case" should "have exit code " + exitCode.toString in {
+            result shouldBe (exitCode)
+        }
+    }
+
+    def runTestOnDirectory(path: String): Unit = {
+        Files.walk(Paths.get("wacc_examples/" + path)).iterator().asScala.filter(Files.isRegularFile(_)).foreach(runTest)
+    }
+  
+}
