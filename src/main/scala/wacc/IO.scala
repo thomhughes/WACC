5e3d41b1c306389abb7c894367f760ba3fc1d7dc
Sachin Wadhwani <sachin.wadhwani21@imperial.ac.uk>
2023-02-06 14:08:06 +0000

    Applied fix for default CharSet problem

--- src/main/scala/wacc/IO.scala
+++ src/main/scala/wacc/IO.scala
@@ -2,10 +2,12 @@ package wacc
 
 object io {
   import scala.io.Source
+  import scala.io.Codec
 
   def readFile(fileName: String): String = {
+    implicit val codec: Codec = Codec.UTF8
     val source = Source.fromFile(fileName)
-    try source.getLines.mkString("\n")
+    try source.getLines().mkString("\n")
     finally source.close
   }
 
