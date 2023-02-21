0d4b3bec549dc7486d9bc21a990db8ca8080a94d
Dhruv Jimulia <dj321@ic.ac.uk>
2023-02-21 18:56:48 +0000

    Added some more unit tests for print statements, refactored instructions in unit tests to use default None values in Instr data type

--- src/test/scala/wacc/IRConversionUnitTests.scala
+++ src/test/scala/wacc/IRConversionUnitTests.scala
@@ -16,37 +16,61 @@ class IRConversionUnitTests extends AnyFlatSpec with BeforeAndAfterEach {
     "IR Conversion" should "generate the correct IR for an AST for a basic declaration" in {
         val program = parseAsProgram("begin int x = 3 end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV, Some(R8), Some(Imm(3))), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(STR,Some(Var("x")),Some(R8),None,AL)))
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV, Some(R8), Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
     }
 
     "IR Conversion" should "generate the correct IR for an AST for a basic reassignment" in {
         val program = parseAsProgram("begin int x = 3; x = 4 end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV, Some(R8), Some(Imm(3))), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(STR,Some(Var("x")),Some(R8),None,AL), Instr(MOV, Some(R8), Some(Imm(4))), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(STR,Some(Var("x")),Some(R8),None,AL)))
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV, Some(R8), Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8)), Instr(MOV, Some(R8), Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
     }
 
     "IR Conversion" should "generate the correct IR for an AST for a simple addition" in {
         val program = parseAsProgram("begin int x = 3 + 4 end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R9),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(ADD,Some(R8),Some(R8),Some(R9),AL), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(STR,Some(Var("x")),Some(R8),None,AL)))
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
     }
 
     "IR Conversion" should "generate the correct IR for an AST for a complex addition" in {
         val program = parseAsProgram("begin int x = (3 + 4) + (5 + 6) end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(5))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(6))), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R9),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(ADD,Some(R8),Some(R8),Some(R9),AL), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R9),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(ADD,Some(R8),Some(R8),Some(R9),AL), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R8),None,None,AL), Instr(STR,Some(Var("x")),Some(R8),None,AL)))
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(5))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(6))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
     }
 
     "IR Conversion" should "generate the correct IR for print statements with string literals" in {
         val program = parseAsProgram("begin \n print \"Hello\" \n end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Data(LabelRef(".L.str0"),"Hello"), Instr(LDR,Some(R8),Some(LabelRef(".L.str0")),None,AL), Instr(PUSH,Some(R8),None,None,AL), Instr(POP,Some(R0),None,None,AL), Instr(PRINT(SAStringType),None,None,None,AL)))
+        buildIR(program, symbolTable) should be (ListBuffer(Data(LabelRef(".L.str0"),"Hello"), Instr(LDR,Some(R8),Some(LabelRef(".L.str0"))), Instr(PUSH,Some(R8)), Instr(POP,Some(R0)), Instr(PRINT(SAStringType))))
     }
 
-    "IR Conversion" should "generate the correct IR for an AST for a simple subtraction" in {
-        val program = parseAsProgram("begin int x = 3 - 4 end")
+    "IR Conversion" should "generate the correct IR for print statements with int literals" in {
+        val program = parseAsProgram("begin \n print 3 \n end")
         val (_, symbolTable, _) = checkProgram(program)
-        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(SUB,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(POP,Some(R0)), Instr(PRINT(SAIntType))))
+    }
+
+    "IR Conversion" should "generate the correct IR for print statements with char literals" in {
+        val program = parseAsProgram("begin \n print 'a' \n end")
+        val (_, symbolTable, _) = checkProgram(program)
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(97))), Instr(PUSH,Some(R8)), Instr(POP,Some(R0)), Instr(PRINT(SACharType))))
+    }
+
+    "IR Conversion" should "generate the correct IR for print statements with binary operator application" in {
+        val program = parseAsProgram("begin \n print 3 + 4 \n end")
+        val (_, symbolTable, _) = checkProgram(program)
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R9)), Instr(POP,Some(R8)), Instr(ADD,Some(R8),Some(R8),Some(R9)), Instr(PUSH,Some(R8)), Instr(POP,Some(R0)), Instr(PRINT(SAIntType))))
+    }
+
+    // "IR Conversion" should "generate the correct IR for print statements with variable references" in {
+    //     val program = parseAsProgram("begin int x = 3; print x end")
+    //     val (_, symbolTable, _) = checkProgram(program)
+    //     buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8)), Instr(LDR,Some(R0),Some(Var("x"))), Instr(PRINT(SAIntType))))
+    // }
+
+    "IR Conversion" should "generate the correct IR for reassignment" in {
+        val program = parseAsProgram("begin int x = 3; x = 4 end")
+        val (_, symbolTable, _) = checkProgram(program)
+        buildIR(program, symbolTable) should be (ListBuffer(Instr(MOV,Some(R8),Some(Imm(3))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8)), Instr(MOV,Some(R8),Some(Imm(4))), Instr(PUSH,Some(R8)), Instr(POP,Some(R8)), Instr(STR,Some(Var("x")),Some(R8))))
     }
 
     "IR Conversion" should "generate the correct IR for an AST for a simple multiplication" in {
