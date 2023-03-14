24edd9c0f63a7b919be2df18f380b04e9b5155a0
Thom Hughes <th520@ic.ac.uk>
2023-03-14 04:05:14 +0000

    Merged standard library runtime with garbage collector changes

--- waccyruntime/runtime.h
+++ waccyruntime/runtime.h
@@ -1,7 +1,15 @@
 #ifndef _RUNTIME_H
 #define _RUNTIME_H
 
+struct array
+{
+  void **data;
+  unsigned elemsize;
+  unsigned size;
+};
+
 void array_mark(void *array);
 void pair_mark(void *pair);
+unsigned array_size(struct array *in);
 
 #endif
