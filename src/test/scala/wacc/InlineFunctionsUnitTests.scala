ae4c478cef69188cca54be446ed8428b96e245db
ard21 <ard21@ic.ac.uk>
2023-03-17 03:41:08 +0000

    forgot to push modified test suite for private methods

--- src/test/scala/wacc/InlineFunctionsUnitTests.scala
+++ src/test/scala/wacc/InlineFunctionsUnitTests.scala
@@ -2,13 +2,18 @@ package wacc
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
+import scala.collection.mutable.Map
 import Parser.parseAsProgram
-import IR.getInlinedFunctions
+import org.scalatest.PrivateMethodTester
+import wacc.AST.{Parameter, Statement}
 
-class InlineFunctionsUnitTests extends AnyFlatSpec {
+class InlineFunctionsUnitTests extends AnyFlatSpec with PrivateMethodTester {
+
+  val transform = PrivateMethod[Map[String, (Int, List[Parameter], List[Statement])]]('getInlinedFunctions)
+  
   "getInlinedFunctions" should "inline small functions" in {
     val program = parseAsProgram("begin int foo() is return 1 end int bar() is return 2 end int x = call foo() ; int y = call bar() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set("foo", "bar")
     }
@@ -16,7 +21,7 @@ class InlineFunctionsUnitTests extends AnyFlatSpec {
 
   "getInlinedFunctions" should "inline functions with a small number of function calls" in {
     val program = parseAsProgram("begin int foo() is return 1 end int x = call foo() ; int y = call foo() ; int z = call foo() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set("foo")
     }
@@ -24,7 +29,7 @@ class InlineFunctionsUnitTests extends AnyFlatSpec {
 
   "getInlinedFunctions" should "inline small functions that have a large number of function calls" in {
     val program = parseAsProgram("begin int foo() is int a = 1; return 1 end int b = call foo() ; int c = call foo() ; int d = call foo() ; int e = call foo() ; int f = call foo() ; int g = call foo() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set("foo")
     }
@@ -32,7 +37,7 @@ class InlineFunctionsUnitTests extends AnyFlatSpec {
 
   "getInlinedFunctions" should "inline large functions that have a small number of function calls" in {
     val program = parseAsProgram("begin int foo() is int a = 1; int b = 2; int c = 3; int d = 4; int e = 5; int f = 6; return 1 end int g = call foo() ; int h = call foo() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set("foo")
     }
@@ -40,7 +45,7 @@ class InlineFunctionsUnitTests extends AnyFlatSpec {
 
   "getInlinedFunctions" should "not inline large functions that have a large number of function calls" in {
     val program = parseAsProgram("begin int foo() is int a = 1; int b = 2; int c = 3; int d = 4; int e = 5; int f = 6; return 1 end int g = call foo() ; int h = call foo() ; int i = call foo() ; int j = call foo() ; int k = call foo() ; int l = call foo() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set()
     }
@@ -48,7 +53,7 @@ class InlineFunctionsUnitTests extends AnyFlatSpec {
 
   "getInlinedFunctions" should "not inline nested functions" in {
     val program = parseAsProgram("begin int foo() is int x = call bar () ; return 1 end int bar() is return 1 end int x = call foo() end")
-    val inlinedFunctions = getInlinedFunctions(program)
+    val inlinedFunctions = IR invokePrivate transform(program)
     inlinedFunctions.keySet should be {
         Set("bar")
     }
